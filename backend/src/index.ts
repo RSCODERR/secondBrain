31caf4d64a506e50d4debe54816a2d85ac06d28f
RSCODERR <akasharmaraghav@gmail.com>
2026-01-21 21:58:56 +0530

    Update CORS configuration and change backend URL to production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: "http://localhost:5173",
-    credentials: true
+    credentials: true,
+    methods:['GET','POST','PUT','DELETE']
 }
 ));
 app.use(cookieParser());
